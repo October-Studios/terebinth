5b5bafebc4e7c3c50caf2895abe03f98851b045e
Cameron Howell <jango.djarin@proton.me>
2024-06-23 18:04:18 -0500

    Working on main

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+pub struct Flags {
+    pub path: String,
+    pub in_files: Vec<String>,
+    pub debug: bool,
+    pub help: bool,
+    pub version: bool,
+    pub run_interpreted: bool,
+    pub cpp_out_file: String,
+    pub bin_out_file: String,
+    pub run_compiled: bool,
+    pub flag_error: bool,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let flags: Flags = Flags::new();
+    if flags.flag_error {}
 }
