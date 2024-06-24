6c8239be29ed8f9eb21a71006957d23526228579
Cameron Howell <jango.djarin@proton.me>
2024-06-23 23:59:53 -0500

    Added logger (rust log crate) and some fleshing out of main

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+//     terebinth - lightweight interpreted programming language
+//     Copyright (C) 2024 Cameron Howell
+//
+//     Licensed under the MIT License
+
+use log::{error, info};
+use std::error::Error;
+
+const VERSION: &str = env!("CARGO_PKG_VERSION");
+
+#[derive(Debug)]
 pub struct Flags {
     pub path: String,
     pub in_files: Vec<String>,
@@ -11,7 +22,41 @@ pub struct Flags {
     pub flag_error: bool,
 }
 
-fn main() {
+impl Flags {
+    #[must_use]
+    pub fn new() -> Self {
+        Flags {
+            path: "".to_string(),
+            in_files: vec![],
+            debug: false,
+            help: false,
+            version: false,
+            run_interpreted: true,
+            cpp_out_file: "".to_string(),
+            bin_out_file: "".to_string(),
+            run_compiled: false,
+            flag_error: false,
+        }
+    }
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
     let flags: Flags = Flags::new();
-    if flags.flag_error {}
+    if flags.flag_error {
+        error!("Try 'terebinth -h' for help");
+        return Ok(());
+    }
+
+    if flags.help {
+        println!("terebinth v {}", VERSION);
+        println!();
+        return Ok(());
+    }
+
+    if flags.version {
+        info!("terebinth v {}", VERSION);
+        return Ok(());
+    }
+
+    Ok(())
 }
